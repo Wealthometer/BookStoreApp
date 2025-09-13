47afb1653a376b431ab39d6dd2bf490857968571
Wealthometer <wealthhajoh87@gmail.com>
2025-09-13 17:11:47 +0100

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
+
+console.log({ PORT });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log(`Server is running on port ${PORT}`);
+});
