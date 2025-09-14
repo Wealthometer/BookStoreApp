b676d1379525b026855c5cbbf9321561f925e271
Wealthometer <wealthhajoh87@gmail.com>
2025-09-14 09:51:43 +0100

    update routes and models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from './routes/authRoutes.js';
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 console.log({ PORT });
 
